7121849a08ded6e75935e9beb944c81f5c8c029a
ErMapsh <maheshmestri73@gmail.com>
2022-07-24 21:06:35 +0530

    login api done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,34 +1,39 @@
 const express = require("express");
 const { validationResult, body } = require("express-validator");
-const { exists } = require("../models/UserSchema");
 const router = express.Router();
 const User = require("../models/UserSchema");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+require("dotenv").config({ path: "../config/info.env" });
 
-// https://localhost:3000/api/auth/register
+// https://localhost:3000/api/auth/register dosent require login
 router.post(
   "/register",
   [
-    body("username", "Name Must be at least 3 ").isLength({ min: 3 }),
-    body("email", "Enter a valid email").isEmail(),
-    body("work", "Enter work").isLength({ min: 3 }),
-    body("phone", "Enter a valid phone number").isMobilePhone(),
+    body("username", "Name Must be at least 3 ")
+      .isString()
+      .isLength({ min: 3 }),
+    body("email", "Enter a valid email").isString().isEmail(),
+    body("work", "Enter work").isString().isLength({ min: 3 }),
+    body("phone", "Enter a valid phone number").isNumeric().isMobilePhone(),
     body("password", "Password must be at least 8 ").isLength({ min: 8 }),
     body("Cpassword", "Password must be at least 8 ").isLength({ min: 8 }),
   ],
   async (req, res) => {
-    const { username, email, phone, work, password, Cpassword } = req.body;
+    const { username, email, phone, work, password } = req.body;
 
     try {
       const error = validationResult(req);
-      console.log(error);
+
+      // console.log(error);
       if (!error.isEmpty()) {
         return res
           .status(400)
           .json({ success: false, error: "Invalid Credentials" });
       }
 
-      const exists = await User.findOne({ email: email });
-      if (exists) {
+      const EmailExist = await User.findOne({ email: email });
+      if (EmailExist) {
         res.status(422).json({ success: false, error: "Email alreay Exists" });
       }
 
@@ -40,22 +45,29 @@ router.post(
         });
       }
 
-      userinfo = await User.create({
-        username,
-        email,
-        phone,
-        work,
-        password,
-        Cpassword,
-      });
+      const salt = await bcrypt.genSalt(10); //generating salt
+      const securePass = await bcrypt.hash(password, salt);
 
+      userinfo = await User.create({
+        username: username,
+        email: email,
+        phone: phone,
+        work: work,
+        password: securePass,
+        Cpassword: securePass,
+      });
       await userinfo.save();
 
-      res.json({
+      const payload = { user: { id: userinfo.id } };
+      const authtoken = jwt.sign(payload, process.env.JWT_SECRET_KEY);
+
+      res.status(201).json({
         Success: true,
         message: "Registration/Signup Successful",
+        authtoken: authtoken,
       });
     } catch (err) {
+      console.log(err);
       res.status(500).json({
         success: false,
         msg: "Internal Server Error Occur",
@@ -65,5 +77,62 @@ router.post(
   }
 );
 
-router.post("/login", (req, res) => {});
+// https://localhost:3000/api/auth/login
+router.post(
+  "/login",
+
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank ").exists(),
+  ],
+
+  async (req, res) => {
+    const { email, password } = req.body;
+
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(400)
+        .json({ success: false, error: "invalid Credentials" });
+    }
+
+    try {
+      let UserInfo = await User.findOne({ email: req.body.email });
+      // res.json(UserInfo);
+
+      if (!UserInfo) {
+        res.status(401).json({
+          success: false,
+          error: "Please try to login with correct credentials",
+        });
+      }
+
+      let PasswordCompare = await bcrypt.compare(password, UserInfo.password);
+
+      if (!PasswordCompare) {
+        return res.status(400).json({
+          success: false,
+          error: "Please try to login with correct credentials",
+        });
+      }
+
+      const payload = { user: { id: UserInfo.id } };
+      const authtoken = await jwt.sign(payload, process.env.JWT_SECRET_KEY);
+
+      res.status(200).json({
+        success: true,
+        authtoken: authtoken,
+        msg: "Successfully login...",
+      });
+
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        error: "Internal Server Error Occur",
+        error2: err,
+      });
+    }
+  }
+);
+
 module.exports = router;
