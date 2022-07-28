01f9bee4c1391eab9942b1ab8c00cdc11aa5ecad
ErMapsh <maheshmestri73@gmail.com>
2022-07-28 09:30:42 +0530

    home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
+import "../styles/Home.css";
 
 export default function Home() {
   return (
-    <div className="d-flex">
-      <div></div>
-      <div></div>
-      {/* <p className="pt-5">Welcome</p>
-      <h1>We are the <span class="MernDevText">Mern</span>Developer</h1> */}
+    <div className="Home">
+      <div className="Homebox1">
+        <div className="Homerelative1">Welcome</div>
+      </div>
+      <div className="Homebox2">
+        <div className="Homerelative2">We Are The MERN Developer</div>
+      </div>
     </div>
   );
 }
